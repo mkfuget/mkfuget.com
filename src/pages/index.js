ffb6a800ddb0cd9d8f51abc346a4e03173036ae5
Michael Fuget <michaelfuget@gmail.com>
2025-03-01 17:01:12 -0800

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,24 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
+import Seo from '../components/seo'
+
+const IndexPage = () => {
+  return (
+    <Layout pageTitle="Home Page">
+      <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        alt="Octopus Tentacle"
+        src="..\images\SundRockFeb2025\Octo1.jpg"
+      />
+      <StaticImage
+        alt="Octopus Tentacle"
+        src="..\images\SundRockFeb2025\Octo2.jpg"
+      />
+    </Layout>
+  )
+}
+
+export const Head = () => <Seo title="Home Page" />
+
+export default IndexPage
