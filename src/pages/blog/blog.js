cbc82d92eb1d6e4565ff605d1f4bf89f62dbdc25
Michael Fuget <michaelfuget@gmail.com>
2025-03-02 12:18:56 -0800

    first pass

--- src/pages/blog/blog.js
+++ src/pages/blog/blog.js
@@ -2,6 +2,7 @@ import * as React from 'react'
 import Layout from '../../components/layout'
 import { Link, graphql } from 'gatsby'
 import Seo from '../../components/seo'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 
 const BlogPage = ({ data }) => {
   return (
--- src/pages/blog/{mdx.frontmatter__slug}.js
+++ src/pages/blog/{mdx.frontmatter__slug}.js
@@ -29,7 +29,6 @@ export const query = graphql`
       frontmatter {
         title
         date(formatString: "MMMM DD, YYYY")
-        hero_image_alt
         images {
           childImageSharp {
             gatsbyImageData
